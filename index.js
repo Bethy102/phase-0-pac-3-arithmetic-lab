7af5a8e38fbc63446be2bc4cf9676cf99152b037
Elisabeth Aka <aea536@nyu.edu>
2022-11-03 00:04:03 -0400

    commit tree dea9462b6d8befd16ccd3493ebfd144a3c9aa9f2

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+
+}
+function subtract(a, b) {
+    return a - b;
+
+}
+function multiply(a, b) {
+    return a * b;
+
+}
+function divide(a, b) {
+    return a / b;
+
+}
+number = 10;
+function add5() {
+    return (number += 5);
+}
